86913e4a24d9432bebaed28105aa58580468f7ec
gatoronfire <diego.casas11104@gmail.com>
2023-01-03 14:59:24 -0300

    role changes

--- bot2.v1/commands/get_roles.js
+++ bot2.v1/commands/get_roles.js
@@ -18,7 +18,7 @@ module.exports = //funcion para iniciar
 
         let rolenames = m.map(u => u.roles.cache.filter((roles) => roles.id !== message.guild.id).map((role) => role.name));
 
-        let test = message.member.permissions.has(0x0000000000000001);
+        let test = message.member.permissions.has((1 << 3));
         message.channel.send(test);
 
         /*for(let i=0; i< usernames.length; i++){ 
@@ -28,3 +28,16 @@ module.exports = //funcion para iniciar
     }
 
 }
+/*
+CREATE_INSTANT_INVITE	0x0000000000000001
+KICK_MEMBERS *	0x0000000000000002
+BAN_MEMBERS *	0x0000000000000004
+ADMINISTRATOR *	0x0000000000000008
+MANAGE_CHANNELS *	0x0000000000000010
+MANAGE_GUILD *	0x0000000000000020
+VIEW_AUDIT_LOG	0x0000000000000080
+ADD_REACTIONS	0x0000000000000040
+rest on:
+https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+
+*/
